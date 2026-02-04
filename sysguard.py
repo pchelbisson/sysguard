611449bf86e702032f2ddf60a69ad5ecd8d08480
pchelbisson <iamroypchel@gmail.com>
2026-02-04 05:55:38 +0000

    refactor: transition to modular project structure

--- sysguard.py
+++ sysguard.py
@@ -7,40 +7,7 @@ import os
 import logging
 import json
 import socket
-from logging.handlers import RotatingFileHandler
-
-def setup_logging():
-    """Logging setup: file + console."""
-    
-    # Create a root logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)  # We catch EVERYTHING, filtering at the handler level
-    
-    # Message format
-    formatter = logging.Formatter(
-        fmt='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    
-    # File handler (DEBUG, with rotation)
-    file_handler = RotatingFileHandler(
-        'sysguard.log',
-        maxBytes=1_000_000,  # 1 MB
-        backupCount=5        # Keep 5 old files
-    )
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    
-    # Handler for console (INFO)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    
-    # Adding handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-    
-    logging.debug("Logging initialized")  # It will only get into the file
+from logging_setup import setup_logging 
 
 def handle_error(report_dict, msg, is_critical):
     """Updates the report dictionary and logs the error."""
