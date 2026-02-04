ee1e834c53a8569f8a5fa84211e89e6932a92af5
pchelbisson <iamroypchel@gmail.com>
2026-02-04 05:23:47 +0000

    feat: add mounts checks

--- test_sysguard.py
+++ test_sysguard.py
@@ -1,10 +1,13 @@
 import pytest
 import sys
+import os
+from pathlib import Path
 from unittest.mock import patch
 from sysguard import check_python_version
 from sysguard import check_root
-from pathlib import Path
 from sysguard import check_directory
+from sysguard import check_mount
+
 
 def test_python_version_is_dict():
     """Checking that the function actually returns a dictionary (basic concept)"""
@@ -58,3 +61,29 @@ def test_check_directory_not_found():
     
     assert result["status"] == "WARNING"
     assert "Does not exist" in result["message"]
+    
+import os
+
+
+def test_check_mount_root_ok():
+    result = check_mount("/")
+    
+    assert result["status"] == "OK"
+    assert result["data"]["is_mounted"] is True
+    assert "is a mount point" in result["message"]
+
+def test_check_mount_not_a_mount():
+    """We test a regular folder (for example, /etc) that is not a mount point"""
+    # /etc is just a folder inside the root filesystem, not a separate mount
+    result = check_mount("/etc", is_critical=False)
+    
+    # Here we expect a WARNING, since we passed is_critical=False
+    assert result["status"] == "WARNING"
+    assert result["data"]["is_mounted"] is False
+    assert "is NOT a mount point" in result["message"]
+
+def test_check_mount_critical_failure():
+    """Testing for a critical mounting error"""
+    result = check_mount("/tmp/non_existent_mount", is_critical=True)
+    
+    assert result["status"] == "ERROR"
